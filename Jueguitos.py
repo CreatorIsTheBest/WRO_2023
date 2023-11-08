4f2fb2c57ac3ad602e305782e460239e10a08efd
FranciscoJavierAbajoAlonso <titiabajoalonso@gmail.com>
2023-11-08 19:58:44 +0100

    estudiante 1

--- Jueguitos.py
+++ Jueguitos.py
@@ -1,7 +1,9 @@
 # importacion de libreias
-
+import random
 # funcion numero random
-
+def crear_un_numero_aleatorio():
+    numero_elegido = random(0,100)
+    return numero_elegido
 # funcion principal
 
 # funcion final
