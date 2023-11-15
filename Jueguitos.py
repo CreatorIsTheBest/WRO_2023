119f26eaafd0747dbd00731ef7d9765c7d545e8b
CreatorIsTheBest <148372371+CreatorIsTheBest@users.noreply.github.com>
2023-11-15 18:49:39 +0100

    Update Jueguitos.py

--- Jueguitos.py
+++ Jueguitos.py
@@ -1,11 +1,13 @@
 # importacion de libreias
 import random
 # funcion numero random
+
+global acertado = False
+global Intentos =  0
+
 def crear_un_numero_aleatorio():
     numero_elegido = random(0,100)
     return numero_elegido
-    global acertado = False
-    global Intentos =  0
 # funcion principal
 
 # funcion final
