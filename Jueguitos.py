b1f6d507ee93a838c36d0e53d6aaec773122de98
FranciscoJavierAbajoAlonso <148376519+FranciscoJavierAbajoAlonso@users.noreply.github.com>
2023-11-15 18:42:10 +0100

    Update Jueguitos.py

--- Jueguitos.py
+++ Jueguitos.py
@@ -4,6 +4,8 @@ import random
 def crear_un_numero_aleatorio():
     numero_elegido = random(0,100)
     return numero_elegido
+    global acertado = False
+    global Intentos =  0
 # funcion principal
 
 # funcion final
