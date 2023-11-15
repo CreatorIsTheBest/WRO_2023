b84210c99cdd2194d6f65e1196cf8a64a9a7d38d
CreatorIsTheBest <pprcreator@gmail.com>
2023-11-15 18:44:41 +0100

    Todo reday de velda

--- Jueguitos.py
+++ Jueguitos.py
@@ -8,17 +8,19 @@
 def main():
     Numero_secreto = crear_un_numero_aleatorio()
     Respuesta = input("¿Cual es el nuemro?: ")
-    acertado = False
     while acertado == False:
         if Respuesta >= Numero_secreto:
             print("Mas pequeño")
+            Intentos = Intentos + 1
         if Respuesta <= Numero_secreto:
             print("Mas Grande")
+            Intentos = Intentos + 1
         if Respuesta == Numero_secreto:
             print("Correcto!!!!!")
             acertado = True
             if input("¿Quieres volver a jugar (y/n)?") == "y":
                 print("Genial!")
+                print("Has necesitado: ",Intentos," intentos")
                 main()
 
 if input("¿Quieres jugar? (y/n)") == "y":
