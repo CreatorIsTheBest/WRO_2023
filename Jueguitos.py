aabaacb54fd078bb82159888a2334863fbe4d504
CreatorIsTheBest <pprcreator@gmail.com>
2023-11-15 18:24:30 +0100

    Todo ready

--- Jueguitos.py
+++ Jueguitos.py
@@ -4,4 +4,24 @@
 
 # funcion principal
 
+
+def main():
+    Numero_secreto = crear_un_numero_aleatorio()
+    Respuesta = input("¿Cual es el nuemro?: ")
+    acertado = False
+    while acertado == False:
+        if Respuesta >= Numero_secreto:
+            print("Mas pequeño")
+        if Respuesta <= Numero_secreto:
+            print("Mas Grande")
+        if Respuesta == Numero_secreto:
+            print("Correcto!!!!!")
+            acertado = True
+            if input("¿Quieres volver a jugar (y/n)?") == "y":
+                print("Genial!")
+                main()
+
+if input("¿Quieres jugar? (y/n)") == "y":
+    main()
+
 # funcion final
